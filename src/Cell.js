49d9ab3209d923ec148ccd8c2373e477cb5916d7
anuwatavis <anuwat.avis@gmail.com>
2020-05-03 13:58:11 +0700

    Code Refactoring 2 - Use Functional Component

--- src/Cell.js
+++ src/Cell.js
@@ -1,19 +1,17 @@
-import React, { Component } from "react";
-class Cell extends Component {
-  render() {
-    return (
-      <div
-        onClick={(e) => {
-          if (this.props.isInitial) {
-            return;
-          }
-          this.props.onChange((this.props.number + 1) % 5);
-        }}
-        className={`cell ${this.props.isInitial ? "initial" : ""}`}
-      >
-        {this.props.number !== 0 && this.props.number}
-      </div>
-    );
-  }
-}
+import React from "react";
+
+const Cell = ({ isInitial, number, onChange }) => (
+  <div
+    onClick={(e) => {
+      if (isInitial) {
+        return;
+      }
+      onChange((number + 1) % 5);
+    }}
+    className={`cell ${isInitial ? "initial" : ""}`}
+  >
+    {number !== 0 && number}
+  </div>
+);
+
 export default Cell;
