6d809e11f58c81088c7fdefec5bca11cde54d235
anuwatavis <anuwat.avis@gmail.com>
2020-05-03 12:54:58 +0700

    add sudoku game

--- src/App.js
+++ src/App.js
@@ -1,26 +1,98 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from "react";
+import "./App.css";
+const validate = (board) => {
+  let isValid = true;
+  for (let i = 0; i < 4; i++) {
+    const horizontal = new Set();
+    const vertical = new Set();
+    const square = new Set();
+    for (let j = 0; j < 4; j++) {
+      horizontal.add(board[i][j]);
+      vertical.add(board[j][i]);
+      square.add(board[2 * (i % 2) + (j % 2)][2 * Math.floor(i / 2) + Math.floor(j / 2)]);
+    }
+    horizontal.delete(0);
+    vertical.delete(0);
+    square.delete(0);
+    if (horizontal.size !== 4 || vertical.size !== 4 || square.size !== 4) {
+      isValid = false;
+    }
+  }
+  return isValid;
+};
+class Cell extends Component {
+  render() {
+    return (
+      <div
+        onClick={(e) => {
+          if (this.props.isInitial) {
+            return;
+          }
+          this.props.onChange((this.props.number + 1) % 5);
+        }}
+        className={`cell ${this.props.isInitial ? "initial" : ""}`}
+      >
+        {this.props.number !== 0 && this.props.number}
+      </div>
+    );
+  }
 }
+class Board extends Component {
+  state = {
+    board: [
+      [1, 2, 3, 4],
+      [3, 4, 0, 0],
+      [2, 0, 4, 0],
+      [4, 0, 0, 2],
+    ],
+    initial: [
+      [true, true, true, true],
+      [true, true, false, false],
+      [true, false, true, false],
+      [true, false, false, false],
+    ],
+    statusText: "",
+  };
 
-export default App;
+  submit = () => {
+    const isValid = validate(this.state.board);
+    this.setState({
+      statusText: isValid ? "Correct" : "Incorrect",
+    });
+  };
+  render() {
+    return (
+      <div>
+        <div className="board">
+          {this.state.board.map((row, i) =>
+            row.map((number, j) => (
+              <Cell
+                number={number}
+                key={`cell--`}
+                isInitial={this.state.initial[i][j]}
+                onChange={(newNumber) => {
+                  const { board } = this.state;
+                  board[i][j] = newNumber;
+                  this.setState({
+                    board: board,
+                  });
+                }}
+              />
+            ))
+          )}
+        </div>
+        <button onClick={this.submit}>Submit</button>
+        <p>{this.state.statusText}</p>
+      </div>
+    );
+  }
+}
+export default class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Board />
+      </div>
+    );
+  }
+}
